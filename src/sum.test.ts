c8c2eb326d00619f8b41e59c49334b674cb5fa7d
DHAIRYA GUPTA <122604403+guptadhairy@users.noreply.github.com>
2024-10-18 17:26:30 +0530

    jest with express

---
+++ src/sum.test.ts
@@ -0,0 +1,30 @@
+import { app } from ".";
+import request from "supertest";
+import {describe, it, expect, test} from '@jest/globals';
+
+describe("Post/sum", ()=> {
+    it("should return the sum of two numbers", async()=> {
+        const res = await request(app).post("/sum").send({
+            a:1,
+            b:2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(3);
+    });
+    it("should return the sum of two -ve numbers", async()=> {
+        const res = await request(app).post("/sum").send({
+            a:-1,
+            b:-2
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(-3);
+    });
+    it("should return the sum of two zero numbers", async()=> {
+        const res = await request(app).post("/sum").send({
+            a:0,
+            b:0
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.answer).toBe(0);
+    })
+})
